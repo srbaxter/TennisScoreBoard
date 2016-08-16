85aab90a4d4e3e74e9d8d38a4f22a9e8ef93cbc9
Savanna Baxter <srbaxter@ncsu.edu>
2016-08-16 16:30:36 -0400

    more optimized JS

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-
 	var teams = {
 		teamA: {
 			name: "Team A",
@@ -14,6 +13,8 @@ window.onload = function() {
 			sets: 0		
 		}
 	};
+	var teamA = "teamA";
+	var teamB = "teamB";
 
 	/* Display all scores */
 	function setScore() {
@@ -24,8 +25,23 @@ window.onload = function() {
 			document.getElementById(team + "Sets").innerHTML = "<span>" + teams[team].sets + "</span";	
 		}
 	}
-	setScore();
 
+	/* Changes one teams' point score */
+	function wonPoint(team) {
+		if (teams[team].points === 0) {
+			teams[team].points = 15;		
+		} else if (teams[team].points === 15) {
+			teams[team].points = 30;
+		} else if (teams[team].points === 30) {
+			teams[team].points = 40;
+		} else if (teams[team].points === 40) {
+			teams[team].points = 0;
+			teams[team].games += 1;
+		}
+		setScore();					
+	}
+
+	/* Checks if either team has the correct amount of sets to win the match */
 	function checkwin() {
 		if (teams["teamA"].sets === 2) {
 			alert(teams["teamA"].name + " has won!");
@@ -34,58 +50,30 @@ window.onload = function() {
 		}
 	}
 
-	// function changeScore() {
-	// 	for (var team in teams) {
-	// 		if (teams[team].points === 0) {
-	// 			teams[team].points = 15;		
-	// 		} else if (teams[team].points === 15) {
-	// 			teams[team].points = 30;
-	// 		} else if (teams[team].points === 30) {
-	// 			teams[team].points = 40;
-	// 		}
-	// 		setScore();				
-	// 	}
-	// }
+	/* Initialize the board */
+	setScore();
 
-
+	/* Change score for Team A on click */
 	document.getElementById("teamAPoints").onclick = function() {
-		if (teams["teamA"].points === 0) {
-			teams["teamA"].points = 15;		
-		} else if (teams["teamA"].points === 15) {
-			teams["teamA"].points = 30;
-		} else if (teams["teamA"].points === 30) {
-			teams["teamA"].points = 40;
-		} else if (teams["teamA"].points === 40) {
-			teams["teamA"].points = 0;
-			teams["teamA"].games += 1;
-			if (teams["teamA"].games >= 6 && ( teams["teamA"].games - teams["teamB"].games) >= 2) {
-				teams["teamA"].sets += 1;
-				teams["teamA"].games = 0;	
-			}
+		wonPoint(teamA);
+		if (teams[teamA].games >= 6 && ( teams[teamA].games - teams[teamB].games) >= 2) {
+			teams[teamA].sets += 1;
+			teams[teamA].games = 0;	
 		}
 		setScore();					
 		checkwin();	
 	};
+
+	/* Change score for Team B on click */
 	document.getElementById("teamBPoints").onclick = function() {
-		if (teams["teamB"].points === 0) {
-			teams["teamB"].points = 15;		
-		} else if (teams["teamB"].points === 15) {
-			teams["teamB"].points = 30;
-		} else if (teams["teamB"].points === 30) {
-			teams["teamB"].points = 40;
-		} else if (teams["teamB"].points === 40) {
-			teams["teamB"].points = 0;
-			teams["teamB"].games += 1;		
-			if (teams["teamB"].games >= 6 && ( teams["teamB"].games - teams["teamA"].games) >= 2) {
-				teams["teamB"].sets += 1;
-				teams["teamB"].games = 0;	
-			}		
+		wonPoint(teamB);
+		if (teams[teamB].games >= 6 && ( teams[teamB].games - teams[teamA].games) >= 2) {
+			teams[teamB].sets += 1;
+			teams[teamB].games = 0;	
 		}
-		setScore();			
-		checkwin();	
+		setScore();					
+		checkwin();		
 	};
-
-
 
 	/* Reset all scores when "Reset Score" button is clicked */
 	document.getElementById("reset").onclick = function() {	
@@ -95,5 +83,5 @@ window.onload = function() {
 			teams[team].sets = 0;		
 		} 
 		setScore();
-	}	
+	}
 }
